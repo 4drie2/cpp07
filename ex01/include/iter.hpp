4da33d45b55713977d748465f11cec0ed0e8f81b
Adrien <abidaux@student.42lehavre.fr>
2026-07-11 22:54:47 +0200

    lengh to lenght

--- ex01/include/iter.hpp
+++ ex01/include/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: abidaux <abidaux@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/11 18:37:37 by abidaux           #+#    #+#             */
-/*   Updated: 2026/07/11 20:04:47 by abidaux          ###   ########.fr       */
+/*   Updated: 2026/07/11 22:54:32 by abidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 #include <cstddef>
 
 template <typename T, typename F>
-void iter(T* array, size_t lengh, F function) {
-    if (!array || lengh == 0)
+void iter(T* array, size_t length, F function) {
+    if (!array || length == 0)
         return;
-    for (size_t i = 0; i < lengh; i++) {
+    for (size_t i = 0; i < length; i++) {
         function(array[i]);
     }
 }
 
 template <typename T, typename F>
-void iter(const T* array, size_t lengh, F function) {
-    if (!array || lengh == 0)
+void iter(const T* array, size_t length, F function) {
+    if (!array || length == 0)
         return;
-    for (size_t i = 0; i < lengh; i++) {
+    for (size_t i = 0; i < length; i++) {
         function(array[i]);
     }
 }
